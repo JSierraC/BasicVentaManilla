a1ccf36072c368308cfaa7515b75ba4ec1cbe25f
Jsierr4 <juan.sierracastro@hotmail.com>
2019-08-25 20:08:42 -0500

    implementar string translate

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -17,6 +17,9 @@ import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputLayout;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     Button btn;
     private TextInputLayout textInputCant;
@@ -31,7 +34,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         txtTitu1 = findViewById(R.id.txt1);
         txtTitu2 = findViewById(R.id.txt2);
         txtTitu3 = findViewById(R.id.txt3);
-
+        btn = findViewById(R.id.btnCalcular);
         txtRes = findViewById(R.id.txtResult);
         textInputCant = findViewById(R.id.text_input_cant);
         sp1 = findViewById(R.id.spinner);
@@ -68,10 +71,17 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     }
 
     private boolean validateCantidad(){
-        Integer cant = Integer.parseInt(textInputCant.getEditText().getText().toString());
+        if(textInputCant.getEditText().getText().toString().trim().length()==0){
+            textInputCant.setError(getResources().getString(R.string.errVacio));
+            return false;
+        }else{
+            textInputCant.setError(null);
+        }
 
-        if(cant<=0){
-            textInputCant.setError("ingrese cantidad mayo a 0");
+        Integer cant = Integer.parseInt(textInputCant.getEditText().getText().toString());
+        Log.e("a", String.valueOf(cant));
+        if(cant<=0 ){
+            textInputCant.setError(getResources().getString(R.string.errMayorCero));
             return false;
         }else{
             textInputCant.setError(null);
@@ -85,19 +95,19 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         Integer position3 = (Integer)Integer.parseInt(String.valueOf(sp3.getSelectedItemId()));
 
         if(position==0){
-            txtTitu1.setError("Debe Seleccionar una Opcion");
+            txtTitu1.setError(getResources().getString(R.string.errSelOpcion));
             return false;
         }else{
             txtTitu1.setError(null);
         }
         if(position2==0){
-            txtTitu2.setError("Debe Seleccionar una Opcion");
+            txtTitu2.setError(getResources().getString(R.string.errSelOpcion));
             return false;
         }else{
             txtTitu2.setError(null);
         }
         if(position3==0){
-            txtTitu3.setError("Debe Seleccionar una Opcion");
+            txtTitu3.setError(getResources().getString(R.string.errSelOpcion));
             return false;
         }else{
             txtTitu3.setError(null);
@@ -108,6 +118,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     public void calcular(View v) {
         if(!validateCantidad()){
             return ;
+
         }
 
         if(!validateSelct()){
@@ -117,27 +128,93 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         Integer valueSpinner1 = Integer.parseInt(String.valueOf(sp1.getSelectedItemId()));
         Integer valueSpinner2 = Integer.parseInt(String.valueOf(sp2.getSelectedItemId()));
         Integer valueSpinner3 = Integer.parseInt(String.valueOf(sp3.getSelectedItemId()));
-        Log.d("a", String.valueOf(switchtCambio.isChecked()));
+        Integer cantidad = Integer.parseInt(String.valueOf(textInputCant.getEditText().getText()));
 
+        Integer tasaCambio = 0;
+        if(switchtCambio.isChecked()){
+            tasaCambio= 3200;
+        }else{
+            tasaCambio = 1;
+        }
+
+        Integer precUnit = 0;
         //cond1
         if(valueSpinner1==1 && valueSpinner2==1 && (valueSpinner3==1 || valueSpinner3==3)){
-
+            precUnit=100;
         }
 
         //cond2
-        if(valueSpinner1==1 && valueSpinner2==1 && (valueSpinner3==1 || valueSpinner3==3)){
-
+        if(valueSpinner1==1 && valueSpinner2==1 &&  valueSpinner3==4 ){
+            precUnit=80;
         }
 
         //cond3
-        if(valueSpinner1==1 && valueSpinner2==1 && (valueSpinner3==1 || valueSpinner3==3)){
-
+        if(valueSpinner1==1 && valueSpinner2==1 && valueSpinner3==5){
+            precUnit=70;
         }
+
         //cond4
-        if(valueSpinner1==1 && valueSpinner2==1 && (valueSpinner3==1 || valueSpinner3==3)){
-
+        if(valueSpinner1==1 && valueSpinner2==2 && (valueSpinner3==1 || valueSpinner3==3)){
+            precUnit=120;
         }
 
+        //cond5
+        if(valueSpinner1==1 && valueSpinner2==2 && valueSpinner3==4){
+            precUnit=100;
+        }
+
+
+        //cond6
+        if(valueSpinner1==1 && valueSpinner2==2 && valueSpinner3==5){
+            precUnit=90;
+        }
+
+
+        //cond7
+        if(valueSpinner1==2 && valueSpinner2==1 && (valueSpinner3==1 || valueSpinner3==3)){
+            precUnit=90;
+        }
+
+
+
+        //cond8
+        if(valueSpinner1==2 && valueSpinner2==1 && valueSpinner3==4){
+            precUnit=70;
+        }
+
+
+        //cond9
+        if(valueSpinner1==2 && valueSpinner2==1 && valueSpinner3==5){
+            precUnit=50;
+        }
+
+
+        //cond10
+        if(valueSpinner1==2 && valueSpinner2==2 && (valueSpinner3==1 || valueSpinner3==3)){
+            precUnit=110;
+        }
+
+        //cond11
+        if(valueSpinner1==2 && valueSpinner2==2 && valueSpinner3==4){
+            precUnit=90;
+        }
+
+        //cond12
+        if(valueSpinner1==2 && valueSpinner2==2 && valueSpinner3==5){
+            precUnit=80;
+        }
+
+        Integer valor = cantidad*precUnit*tasaCambio;
+
+        NumberFormat format = NumberFormat.getCurrencyInstance(Locale.US);
+        String currency = format.format(valor);
+
+        if(Locale.getDefault().getLanguage()=="es"){
+            txtRes.setText("La(s) "+ cantidad+ " Manillas Tiene un Valor de "+currency);
+        }
+        if(Locale.getDefault().getLanguage()=="en"){
+            txtRes.setText("The "+cantidad+" bracelets are priced at "+currency);
+        }
     }
 
 
